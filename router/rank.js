0070e16c95760859d344a2672955f3b3cca0e31d
encoreg91785 <encoreg91785@gmail.com>
2019-06-02 14:00:43 +0800

    Add router/rank.js

---
+++ router/rank.js
@@ -0,0 +1,38 @@
+"use strict";
+const express = require('express');  
+const router = express.Router();
+
+const mysql = require('../manager/mysql');
+
+const constData = require('../utility/constData');  
+const response = constData.response;
+const getResTemp = constData.getResTemp;
+
+router.all("/",(req,res,next)=>{
+    console.log(req.baseUrl+'(all)');
+    next();
+});
+
+router.post("/",(req,res)=>{
+    res.send("post");
+});
+
+router.get("/",(req,res)=>{
+    mysql.query("select p.name,p.team, playerqrcode, max(`create`) as lastTime,count(*) as NUM FROM card JOIN player as p on playerqrcode=qrcode GROUP BY playerqrcode ORDER BY NUM DESC ,lasttime ;").then(result=>{
+        res.json(getResTemp(response.successful,result));
+    }).catch(error=>{
+        res.json(getResTemp(response.getError,error));
+    });
+});
+
+router.put("/",(req,res)=>{
+    res.send("put");
+});
+
+router.delete("/",(req,res)=>{
+    res.send("delete");
+});
+
+module.exports={
+    router:router
+}
